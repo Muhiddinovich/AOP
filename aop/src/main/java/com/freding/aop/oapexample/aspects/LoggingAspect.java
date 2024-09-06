53ad8973c0ab422c939e95e2cf673cbe3c1f6f3b
Fakhriddin Mullagoziev <mullagoziev@gmail.com>
2024-09-06 07:36:36 -0400

    Logging Aspect+

---
+++ aop/src/main/java/com/freding/aop/oapexample/aspects/LoggingAspect.java
@@ -0,0 +1,28 @@
+package com.freding.aop.oapexample.aspects;
+
+import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Before;
+import org.aspectj.lang.annotation.Pointcut;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.context.annotation.Configuration;
+
+//Configuration
+//AOP
+@Configuration
+@Aspect
+public class LoggingAspect {
+
+
+	private Logger logger = LoggerFactory.getLogger(getClass());
+
+	// Pointcut - When?
+	
+	// execution(*PACKAGE.*.*(..))
+	@Before("execution(* com.freding.aop.oapexample.business.*.*(..))")
+	public void logMethodCall(JoinPoint joinpoint) {
+		// Logic - What?
+		logger.info("Before Aspect - Method is called - {}", joinpoint);
+	}
+}
