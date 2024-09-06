97588ab2a30a44262f032d78eafce02bd3533e83
Fakhriddin Mullagoziev <mullagoziev@gmail.com>
2024-09-06 03:36:23 -0400

    Setting up Spring Components for Spring AOP

--- aop/src/main/java/com/freding/aop/AopApplication.java
+++ aop/src/main/java/com/freding/aop/AopApplication.java
@@ -1,13 +1,30 @@
 package com.freding.aop;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import com.freding.aop.oapexample.business.BusinessService1;
+
 @SpringBootApplication
-public class AopApplication {
+public class AopApplication implements CommandLineRunner{
+	private Logger logger = LoggerFactory.getLogger(getClass());
+	
+	private BusinessService1 businessService1;
+	
+	public AopApplication(BusinessService1 businessService1) {
+		this.businessService1=businessService1;
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(AopApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		logger.info("Value returned is {}", businessService1.calculateMax());
 	}
 
 }
